67a45d404d39f2e97ee311f79e65e5d88506e04b
hemantic <artem.kiselev@gmail.com>
2020-01-28 00:51:03 +0300

    Added start command and unit tests set for testing commands

--- tests/test_bot_commands.py
+++ tests/test_bot_commands.py
@@ -1,11 +1,11 @@
-import pytest
 import json
+from unittest.mock import patch
 
-from unittest.mock import MagicMock, patch
+import pytest
 from envparse import env
 
 
-def _telegram_json(message=None):
+def _telegram_json_message(message=None):
     with open('tests/mocks/tg_request_text.json') as f:
         json_body = f.read()
         json_data = json.loads(json_body)
@@ -16,9 +16,21 @@ def _telegram_json(message=None):
         return json.dumps(json_data)
 
 
+def _telegram_json_command(command=None):
+    with open('tests/mocks/tg_request_command.json') as f:
+        json_body = f.read()
+        json_data = json.loads(json_body)
+
+        if command is not None:
+            json_data['message']['text'] = command
+            json_data['message']['entities'][0]['length'] = len(command)
+
+        return json.dumps(json_data)
+
+
 @patch('telegram.Message.reply_text')
 def test_command_rnd(mocked_reply_text, web_app):
-    telegram_json = _telegram_json(message='Как дела, потомки?')
+    telegram_json = _telegram_json_message(message='Как дела, потомки?')
 
     web_app.simulate_post('/' + env('TELEGRAM_API_TOKEN'), body=telegram_json)
 
@@ -32,7 +44,7 @@ def test_command_rnd(mocked_reply_text, web_app):
 @patch('src.tasks.schedule_category_export.delay')
 @patch('telegram.Message.reply_text')
 def test_command_catalog(mocked_reply_text, mocked_celery_delay, web_app, message, expected):
-    telegram_json = _telegram_json(message=message)
+    telegram_json = _telegram_json_message(message=message)
 
     web_app.simulate_post('/' + env('TELEGRAM_API_TOKEN'), body=telegram_json)
 
@@ -41,3 +53,12 @@ def test_command_catalog(mocked_reply_text, mocked_celery_delay, web_app, messag
 
     if expected == 'celery_delay':
         mocked_celery_delay.assert_called()
+
+
+@patch('telegram.Message.reply_text')
+def test_command_start(mocked_reply_text, web_app):
+    telegram_json = _telegram_json_command(command='/start')
+
+    web_app.simulate_post('/' + env('TELEGRAM_API_TOKEN'), body=telegram_json)
+
+    mocked_reply_text.assert_called()
