bbc85a6ece8aa0399cdc1ab849a01e29f76b8180
hemantic <artem.kiselev@gmail.com>
2020-02-23 19:28:35 +0300

    Added app log files handler

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,6 @@
 import logging
 
+import json_log_formatter
 import sentry_sdk
 from envparse import env
 from sentry_sdk.integrations.celery import CeleryIntegration
@@ -13,7 +14,14 @@ env.read_envfile()
 logging.basicConfig(format='[%(asctime)s][%(levelname)s] %(message)s',
                     level=logging.INFO)
 
+formatter = json_log_formatter.JSONFormatter()
+
+json_handler = logging.FileHandler(filename='/var/log/wildsearch_app_log.json')
+json_handler.setFormatter(formatter)
+
 logger = logging.getLogger(__name__)
+logger.addHandler(json_handler)
+
 
 # включаем Sentry
 if env('SENTRY_DSN', default=None) is not None:
