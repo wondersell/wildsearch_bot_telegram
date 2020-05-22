d11a8842d2ceb93a28f0e7a962ba278d5c088c7c
hemantic <artem.kiselev@gmail.com>
2020-05-22 23:53:26 +0300

    Returned countdown for data retrieve task start

--- src/web.py
+++ src/web.py
@@ -25,6 +25,7 @@ class CallbackWbCategoryExportResource(object):
                     'job_id': req.get_param('job_id'),
                     'chat_id': req.get_param('chat_id'),
                 },
+                countdown=10,
             )
 
             resp.status = falcon.HTTP_200
