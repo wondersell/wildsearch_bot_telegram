edd6f475b043083837a9cc465e2332532b70a818
hemantic <artem.kiselev@gmail.com>
2020-07-04 04:01:51 +0300

    Added helper functions for numeric handling

--- src/helpers.py
+++ src/helpers.py
@@ -1,6 +1,7 @@
 import json
 import logging
 import re
+import math
 
 import boto3
 import requests
@@ -90,11 +91,7 @@ def category_export(url, chat_id, spider='wb') -> str:
 
 
 def smart_format_number(number):
-    # 177 шт.
-    # 2 112
-    # 4 200 шт.
-    # 4 500 шт.
-    # 6 800 шт.
+
     # 10 650 руб.
     # 15 тыс. шт.
     # 53 тыс
@@ -106,47 +103,151 @@ def smart_format_number(number):
 
     # 892 977 678 236 940
     try:
-        digits = len(str(abs(int(number))))
+        number = int(number)
     except ValueError:
         return '–', ''
 
-    text_digits = ''
+    try:
+        rounded, natural = smart_format_round(number)
+    except ValueError:
+        return '–', ''
 
+    pretty = smart_format_prettify(rounded)
+    text_digits = get_digits_text(len(str(number)))
+
+    return pretty, text_digits
+
+
+def smart_format_round(number) -> (int, int):
+    digits = len(str(abs(number)))
+
+    rounded = round(number)
+    natural = round(number)
+
+    # 177 -> 180
     if 1 < digits <= 3:
-        number = round(number / 10) * 10
+        rounded = round(number / 10) * 10
+        natural = rounded
 
+    # 2 112 -> 2 100
     if 3 < digits <= 4:
-        number = round(number / 100) * 100
+        rounded = round(number / 100) * 100
+        natural = rounded
 
+    # 15 487 -> 15 тыс.
     if 4 < digits <= 6:
-        number = round(number / 1000)
-        text_digits = 'тыс.'
+        rounded = round(number / 1000)
+        natural = rounded * 1000
 
+    # 2 863 578 -> 2,9 млн.
     if 6 < digits <= 8:
-        number = round(number / 1000000, 1)
-        text_digits = 'млн.'
+        rounded = round(number / 1000000, 1)
+        natural = rounded * 1000000
 
+    # 672 934 573 -> 673 млн.
     if 8 < digits <= 9:
-        number = round(number / 1000000)
-        text_digits = 'млн.'
+        rounded = round(number / 1000000)
+        natural = rounded * 1000000
 
+    # 72 691 235 664 -> 72,7 млрд.
     if 9 < digits <= 11:
-        number = round(number / 1000000000, 1)
-        text_digits = 'млрд.'
+        rounded = round(number / 1000000000, 1)
+        natural = rounded * 1000000000
 
+    # 684 971 367 849 -> 685 млрд.
     if 11 < digits <= 12:
-        number = round(number / 1000000000)
-        text_digits = 'млрд.'
+        rounded = round(number / 1000000000)
+        natural = rounded * 1000000000
 
+    # 81 235 118 364 583 -> 81,2 трлн.
     if 12 < digits <= 14:
-        number = round(number / 1000000000000, 1)
-        text_digits = 'трлн.'
+        rounded = round(number / 1000000000000, 1)
+        natural = rounded * 1000000000000
 
+    # 811 135 017 356 193 -> 811 трлн.
     if 14 < digits <= 15:
-        number = round(number / 1000000000000)
-        text_digits = 'трлн.'
+        rounded = round(number / 1000000000000)
+        natural = rounded * 1000000000000
 
+    return rounded, natural
+
+
+def smart_format_round_hard(number) -> (int, int):
+    number = round(number)
+    digits = len(str(abs(number)))
+    remainder = digits % 3
+
+    diveder_degree = 0
+    multiplier = 1
+
+    # Числа из одной цифры просто округляем, для остальных делаем так, чтобы они заканчивались на 0
+    if remainder == 0 and digits > 1:
+        diveder_degree = digits - 2
+        multiplier = 10
+
+    if remainder == 1 and digits > 1:
+        diveder_degree = digits - 1
+        multiplier = 1
+
+    if remainder == 2 and digits > 1:
+        diveder_degree = digits - 1
+        multiplier = 10
+
+    rounded = round(number / 10 ** diveder_degree) * multiplier
+    natural = round(number / 10 ** diveder_degree) * 10 ** diveder_degree
+
+    return rounded, natural
+
+
+def smart_format_round_super_hard(number) -> (int, int):
+    number = round(number)
+    digits = len(str(abs(number)))
+    remainder = digits % 3
+
+    divider_degree = 0
+    multiplier = 1
+
+    # Числа из одной цифры просто округляем, для остальных делаем так, чтобы они заканчивались на 0, округляя вверх
+    if remainder == 1 and digits > 1:
+        divider_degree = digits - 1
+        multiplier = 1
+
+    if remainder == 2 and digits > 1:
+        divider_degree = digits - 1
+        multiplier = 10
+
+    if remainder == 0 and digits > 1:
+        divider_degree = digits - 1
+        multiplier = 100
+
+    rounded = math.ceil(number / 10 ** divider_degree) * multiplier
+    natural = math.ceil(number / 10 ** divider_degree) * 10 ** divider_degree
+
+    return rounded, natural
+
+
+def smart_format_prettify(number):
     number = int(number) if float(round(number, 2)) % 1 == 0 else round(float(number), 2)
     number = '{:,}'.format(number).replace(',', ' ').replace('.', ',')
 
-    return number, text_digits
+    return number
+
+
+def get_digits_text(number_length: int, skip_thousands: bool = True) -> str:
+    if skip_thousands:
+        if 4 < number_length <= 6:
+            return 'тыс.'
+    else:
+        if 3 < number_length <= 6:
+            return 'тыс.'
+
+    if 6 < number_length <= 9:
+        return 'млн.'
+
+    if 9 < number_length <= 12:
+        return 'млрд.'
+
+    if 12 < number_length <= 15:
+        return 'трлн.'
+
+    return ''
