cc8b721de23285d9135470322ba355f330fb930f
hemantic <artem.kiselev@gmail.com>
2020-10-20 22:08:57 +0300

    Added simple command for bulk message sending

--- src/commands/send_message.py
+++ src/commands/send_message.py
@@ -12,14 +12,19 @@ bot = Bot(env('TELEGRAM_API_TOKEN'))
 @click.command()
 @click.argument('text')
 @click.option(
-    '--chat-ids', '-a',
-    help='your API key for the OpenWeatherMap API',
+    '--chat-ids', '-i',
+    help='chat IDs for bulk sending',
 )
 def main(text, chat_ids):
     chat_ids = str.split(chat_ids, ',')
 
     for chat_id in chat_ids:
-        bot.send_message(chat_id=chat_id, text=text, parse_mode='Markdown', disable_web_page_preview=True)
+        try:
+            bot.send_message(chat_id=chat_id, text=text, parse_mode='Markdown', disable_web_page_preview=True)
+
+            print(f'Sending message to chat {chat_id} success')
+        except Exception as error:
+            print(f'Sending message to chat {chat_id} failed: {str(error)}')
 
 
 if __name__ == '__main__':
